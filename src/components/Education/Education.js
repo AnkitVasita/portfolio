935251fd745e6b951abfd2039daa3ee56ed2ad96
AnkitVasita <vasitaankit97@gmail.com>
2022-03-29 21:24:05 +0530

    changes styles

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,6 +1,10 @@
 import { useContext } from "react";
 import "./Education.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import eduImgWhite from "../../assets/svg/education/eduImgWhite.svg";
+import eduImgBlack from "../../assets/svg/education/eduImgBlack.svg";
+import expImgWhite from "../../assets/svg/experience/expImgWhite.svg";
+import expImgBlack from "../../assets/svg/experience/expImgBlack.svg";
 
 function Education() {
   const { theme } = useContext(ThemeContext);
@@ -9,14 +13,30 @@ function Education() {
       <div className="edu-edu">
         <div className="my-education">
           <h1>Education</h1>
-          <h3>Arya Institute of Engineering and Technolgy, Jaipur(Raj)</h3>
+          <div className="edu-icon" style={{ backgroundColor: theme.primary }}>
+            {" "}
+            <img
+              src={theme.type === "dark" ? eduImgBlack : eduImgWhite}
+              // style={{ width: "30%" }}
+              alt=""
+            />
+            <h3>Arya Institute of Engineering and Technolgy, Jaipur(Raj)</h3>
+          </div>
+
           <p>Bachelor of Technology in Computer Science</p>
           <p>2014 - 2018</p>
           {/* <div className="edu-border"></div> */}
         </div>
         <div className="my-education">
           <h1>Work Experience</h1>
-          <h3>SpazioX Private Limited ,Pune</h3>
+          <div className="edu-icon">
+            <img
+              src={theme.type === "dark" ? expImgBlack : expImgWhite}
+              alt=""
+            />
+            <h3>SpazioX Private Limited ,Pune</h3>
+          </div>
+
           <p>Front End Developer</p>
           <p>oct 2019 - May 2020 </p>
           <p>
